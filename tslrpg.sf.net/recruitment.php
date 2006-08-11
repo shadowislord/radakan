fce333ce0299866d3edef699011b31346ee3287f
Taldor <Taldor@06f53981-2c4c-0410-8a1d-d54e5c4dde6c>
2006-08-11 12:03:19 +0000

    htdocs added in order to more easily sync to the website.

--- tslrpg.sf.net/recruitment.php
+++
@@ -1,8 +0,0 @@
-<?php
-	include ("start.txt");
-?>
-	<h1>-Recruitment-</h1>
-	All recruitement details can be found in <a href=http://www.gamedev.net/community/forums/topic.asp?topic_id=406871">our recruitement thread on Gamedev.net</a>.<br/>
-<?php
-	include ("end.txt");
-?>
