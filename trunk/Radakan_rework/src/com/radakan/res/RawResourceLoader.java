fc3ff341e751693bdd83e19cb8f6a8064a931248
jm4games <jm4games@06f53981-2c4c-0410-8a1d-d54e5c4dde6c>
2008-08-07 01:02:30 +0000

    Finished importing orig stuff for most part

---
+++ trunk/Radakan_rework/src/com/radakan/res/RawResourceLoader.java
@@ -0,0 +1,12 @@
+package com.radakan.res;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class RawResourceLoader implements ResourceLoader<InputStream> {
+
+    public InputStream load(String resourceName) throws IOException {
+        return ResourceManager.getFileSystem().openStream(AccessMode.RAW, resourceName);
+    }
+
+}
