ea781d731eebd888be80a41c8a6719a03ab7150f
shadow1500 <shadowislord@gmail.com>
2008-08-18 18:22:16 +0000

    previewtool export formats + moving stuff for tile system

--- World_Tool/src/com/gibbon/tools/world/TerrainUtil.java
+++ World_Tool/src/com/gibbon/tools/world/TerrainUtil.java
@@ -7,61 +7,12 @@ import com.jme.scene.Spatial;
 import com.jme.scene.TexCoords;
 import com.jme.scene.TriMesh;
 import com.jme.util.geom.BufferUtils;
+import com.radakan.game.tile.GridUtil;
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
 
 public class TerrainUtil {
 
-    private static FloatBuffer writeVertexArray(int width, int height, Vector3f scale){
-        FloatBuffer store = BufferUtils.createFloatBuffer(width*height*3);
-
-        // going through Y/Z first, scanlines are horizontal in TLOD
-        for (int z = 0; z < height; z++){
-            for (int x = 0; x < width; x++){
-                store.put( (float)x*scale.x );
-                store.put( (float)0.0*scale.y );
-                store.put( (float)z*scale.z );
-            }
-        }
-        
-        return store;
-    }
-    
-    private static IntBuffer writeIndexArray(int width, int height){
-        int faceN = (width-1)*(height-1)*2;
-        
-        IntBuffer store = BufferUtils.createIntBuffer(faceN*3);
-
-        int i = 0;
-        for (int z = 0; z < height-1; z++){
-            for (int x = 0; x < width-1; x++){
-                store.put(i).put(i+width).put(i+width+1);
-                store.put(i+width+1).put(i+1).put(i);
-                i++;
-                
-                if (x==width-2) i++;
-            }
-        }
-        store.flip();
-        
-        return store;
-    }
-   
-    private static FloatBuffer writeNormalArray(int width, int height) {
-        FloatBuffer store = BufferUtils.createFloatBuffer(width*height*3);
-
-        //Vector3f temp = new Vector3f();
-        for (int z = 0; z < height; z++){
-            for (int x = 0; x < width; x++){
-//                getNormal(x,z,temp);
-//                store.put(temp.x).put(temp.y).put(temp.z);
-                store.put(0.0f).put(1.0f).put(0.0f);
-            }
-        }
-        
-        return store;
-    }
-    
     public static void rebuildNormalArray(Tile tile){
         TriMesh mesh = tile.getTerrain();
         FloatBuffer vb = mesh.getVertexBuffer();
@@ -173,31 +124,7 @@ public class TerrainUtil {
         tile.clearModified();
     }
     
-    private static FloatBuffer writeTexCoordArray(int width, int height, int tWidth, int tHeight, Vector2f offset, Vector2f scale){
-        FloatBuffer store = BufferUtils.createFloatBuffer(width*height*2);
-        
-        if (offset == null)
-            offset = new Vector2f(0f, 0f);
-        
-        if (scale == null)
-            scale = new Vector2f(1f, 1f);
-        
-        Vector2f tcStore = new Vector2f();
-        for (int y = 0; y < height; y++){
-            for (int x = 0; x < width; x++){
-                tcStore.set( (float)x / (float)tWidth,
-                             (float)y / (float)tHeight );
-                store.put( offset.x + tcStore.x * scale.x );
-                store.put( offset.y + tcStore.y * scale.y );
-                
-//                store.put( ((float)x) / getWidth() );
-//                store.put( ((float)y) / getHeight() );
-            }
-            
-        }
-        
-        return store;
-    }
+    
     
     public static TriMesh createGrid(int width, int height, Vector3f scale, Tile target) {
         int groupSize = World.getWorld().getGroupSize();
@@ -215,11 +142,11 @@ public class TerrainUtil {
         
         TriMesh store = new TriMesh("TERRAIN");
 
-        store.setVertexBuffer(writeVertexArray(width,height,scale));
-        store.setNormalBuffer(writeNormalArray(width,height));
-        store.setIndexBuffer(writeIndexArray(width,height));
+        store.setVertexBuffer(GridUtil.writeVertexArray(width,height,scale));
+        store.setNormalBuffer(GridUtil.writeNormalArray(width,height));
+        store.setIndexBuffer(GridUtil.writeIndexArray(width,height));
         
-        FloatBuffer tc = writeTexCoordArray(width, 
+        FloatBuffer tc = GridUtil.writeTexCoordArray(width, 
                                             height, 
                                             width-1,
                                             height-1,
