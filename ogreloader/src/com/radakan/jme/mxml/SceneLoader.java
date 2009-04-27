7e43f8328a852922d7bdbb974285d95d67d1982e
blaine.dev <blaine.dev@06f53981-2c4c-0410-8a1d-d54e5c4dde6c>
2009-04-27 01:21:21 +0000

    Implemented a material URI loader method

--- ogreloader/src/com/radakan/jme/mxml/SceneLoader.java
+++ ogreloader/src/com/radakan/jme/mxml/SceneLoader.java
@@ -399,7 +399,7 @@ public class SceneLoader {
      * </P>
      *
      * @see #getScene()
-     * @see SceneLoader.RelativeResourceLocator
+     * @see RelativeResourceLocator
      */
     public void load(URI uri) throws IOException{
         URL url = uri.toURL();
@@ -431,9 +431,18 @@ public class SceneLoader {
                 ResourceLocatorTool.TYPE_TEXTURE, locator);
         ResourceLocatorTool.addResourceLocator(
                 ResourceLocatorTool.TYPE_MODEL, locator);
+        InputStream stream = null;
         try {
-            load(url.openStream());
+            stream = url.openStream();
+            if (stream == null) {
+                throw new IOException("Failed to load model file '"
+                        + url + "'");
+            }
+            logger.fine("Loading materials from '" + url + "'...");
+            load(stream);
+            stream.close();
         } finally {
+            if (stream != null) stream.close();
             ResourceLocatorTool.removeResourceLocator(
                     ResourceLocatorTool.TYPE_TEXTURE, locator);
             ResourceLocatorTool.removeResourceLocator(
